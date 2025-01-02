bd5835a687cb85a84262c3fd82e8a0a7337ed475
bansalriyaa <152080358+bansalriyaa@users.noreply.github.com>
2025-01-02 18:04:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,93 +1,86 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { CssBaseline, Grid } from '@material-ui/core';
 
-import { getPlacesData, getWeatherData } from './api/travelAdvisorAPI';
-import Header from './components/Header/Header';
-import List from './components/List/List';
-import Map from './components/Map/Map';
+import { getPlacesData, getWeatherData } from "./api";
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
+import { LoadScript } from "@react-google-maps/api";
 
 const App = () => {
-  const [type, setType] = useState('restaurants');
-  const [rating, setRating] = useState('');
+    const [places, setPlaces] = useState([]);
+    const [filteredPlaces, setFilteredPlaces] = useState([]);
+    const [coordinates, setCoordinates] = useState({});
+    const [bounds, setBounds] = useState({});
+    const [childClicked, setChildClicked] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const [type, setType] = useState('restaurants');
+    const [rating, setRating] = useState(0);
+    const [weatherData, setWeatherData] = useState([]);
+    const [autocomplete, setAutocomplete] = useState(null);
 
-  const [coords, setCoords] = useState({});
-  const [bounds, setBounds] = useState(null);
-
-  const [weatherData, setWeatherData] = useState([]);
-  const [filteredPlaces, setFilteredPlaces] = useState([]);
-  const [places, setPlaces] = useState([]);
-
-  const [autocomplete, setAutocomplete] = useState(null);
-  const [childClicked, setChildClicked] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
-      setCoords({ lat: latitude, lng: longitude });
-    });
-  }, []);
-
-  useEffect(() => {
-    const filtered = places.filter((place) => Number(place.rating) > rating);
-
-    setFilteredPlaces(filtered);
-  }, [rating]);
-
-  useEffect(() => {
-    if (bounds) {
-      setIsLoading(true);
-
-      getWeatherData(coords.lat, coords.lng)
-        .then((data) => setWeatherData(data));
-
-      getPlacesData(type, bounds.sw, bounds.ne)
-        .then((data) => {
-          setPlaces(data.filter((place) => place.name && place.num_reviews > 0));
-          setFilteredPlaces([]);
-          setRating('');
-          setIsLoading(false);
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
+            setCoordinates({ lat: latitude, lng: longitude });
         });
-    }
-  }, [bounds, type]);
+    }, []);
 
-  const onLoad = (autoC) => setAutocomplete(autoC);
+    useEffect(() => {
+        setFilteredPlaces(places.filter((place) => place.rating > rating));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [rating]);
+    useEffect(() => {
+        if (bounds.sw && bounds.ne) {
+            setIsLoading(true);
+            getWeatherData(coordinates.lat, coordinates.lng).then((data) => setWeatherData(data));
+            getPlacesData(type, bounds.sw, bounds.ne).then((data) => {
+                setPlaces(data);
+                setFilteredPlaces([]);
+                setIsLoading(false);
+            });
+        }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [type, bounds]);
 
-  const onPlaceChanged = () => {
-    const lat = autocomplete.getPlace().geometry.location.lat();
-    const lng = autocomplete.getPlace().geometry.location.lng();
+    const onLoad = (autoC) => setAutocomplete(autoC);
 
-    setCoords({ lat, lng });
-  };
+    const onPlaceChanged = () => {
+        const lat = autocomplete.getPlace().geometry.location.lat();
+        const lng = autocomplete.getPlace().geometry.location.lng();
+        setCoordinates({ lat, lng });
+    };
 
-  return (
-    <>
-      <CssBaseline />
-      <Header onPlaceChanged={onPlaceChanged} onLoad={onLoad} />
-      <Grid container spacing={3} style={{ width: '100%' }}>
-        <Grid item xs={12} md={4}>
-          <List
-            isLoading={isLoading}
-            childClicked={childClicked}
-            places={filteredPlaces.length ? filteredPlaces : places}
-            type={type}
-            setType={setType}
-            rating={rating}
-            setRating={setRating}
-          />
-        </Grid>
-        <Grid item xs={12} md={8} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-          <Map
-            setChildClicked={setChildClicked}
-            setBounds={setBounds}
-            setCoords={setCoords}
-            coords={coords}
-            places={filteredPlaces.length ? filteredPlaces : places}
-            weatherData={weatherData}
-          />
-        </Grid>
-      </Grid>
-    </>
-  );
+    return (
+        <React.Fragment>
+            <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY} libraries={["places", "geometry", "drawing"]}>
+                <CssBaseline />
+                <Header onLoad={onLoad} onPlaceChanged={onPlaceChanged} />
+                <Grid container spacing={3} style={{ width: '100%' }}>
+                    <Grid item xs={12} md={4}>
+                        <List
+                            places={filteredPlaces.length ? filteredPlaces : places}
+                            childClicked={childClicked}
+                            isLoading={isLoading}
+                            type={type}
+                            setType={setType}
+                            rating={rating}
+                            setRating={setRating}
+                        />
+                    </Grid>
+                    <Grid item xs={12} md={8}>
+                        <Map
+                            setCoordinates={setCoordinates}
+                            setBounds={setBounds}
+                            coordinates={coordinates}
+                            places={filteredPlaces.length ? filteredPlaces : places}
+                            setChildClicked={setChildClicked}
+                            weatherData={weatherData}
+                        />
+                    </Grid>
+                </Grid>
+            </LoadScript>
+        </React.Fragment>
+    );
 };
 
 export default App;
